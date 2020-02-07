6aae905441007345e74a792ac6d79f052adbf9b2
Mehnaz <mehnazahmed1122@gmail.com>
2020-02-07 20:01:27 +0000

    testing2Header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Header from './Header';
+
 class App extends React.Component {
   render() {
     return (
-      <p> Hello there!!! </p>
+      <div>
+        <Header/>
+        <p> Hello there!!! </p>
+      </div>
+      
     );
   }
 }
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+      return (
+        <div> 
+            <h1> this is my title </h1>
+            <h2>this is my subtitle</h2>
+        </div>
+      );
+    }
+  }
+  
+  
+  export default Header;
