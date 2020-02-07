2f103e529c04c608e9ff9a04eb8c58467c8d0edf
Mehnaz <mehnazahmed1122@gmail.com>
2020-02-07 20:44:54 +0000

    HeaderComponent

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Header from './Header';
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className= "container">
         <Header/>
         <p> Hello there!!! </p>
       </div>
--- src/Header.js
+++ src/Header.js
@@ -4,12 +4,19 @@ class Header extends React.Component {
     render() {
       return (
         <div> 
-            <h1> this is my title </h1>
-            <h2>this is my subtitle</h2>
+            <div className="row none md-block" >
+                <div className="col-12 header">
+                    <h1 className="title">My To Do List</h1>
+                    <h6 className="sub-title">Organisation is the key to success</h6>
+                </div>
+            </div>
         </div>
       );
     }
   }
-  
+  // 
   
   export default Header;
+
+  //Mistakes made: 
+  //<h6 className="sub-title" style= "margin-top:-80px;" >Organisation is the key to success</h6>
