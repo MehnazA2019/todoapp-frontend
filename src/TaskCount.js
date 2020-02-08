7b386f57e957959a37a90e7a7adc11a3bea2f487
Mehnaz <mehnazahmed1122@gmail.com>
2020-02-08 20:34:32 +0000

    TaskCountCSSAdded

--- src/TaskCount.js
+++ src/TaskCount.js
@@ -1,95 +1,95 @@
 import React from 'react';
 //five tasks to be added here
 
+import './TaskCount.css'
+
 class TaskCount extends React.Component {
     render() {
       return (
+          <div className="body">
  
-        <div class="todo-body">
-        <div class="col-12 col-md-8">
-          <div class="tasks">
-            <div class="task">
+        <div className="todo-body">
+        <div className="col-12 col-md-8">
+          <div className="tasks">
+            <div className="task">
               <input 
                 type="checkbox"
                 id="task-1"
               />
               <label for="task-1">
-                <span class="custom-checkbox"></span>
+                <span className="custom-checkbox"></span>
                 Task 1
               </label>
-                          <div class="col-6 col-md-2">
-                  <button type="button" class="btn btn-danger">Delete</button>
+                          <div className="col-6 col-md-2">
+                  <button type="button" className="btn btn-danger">Delete</button>
               </div>
             </div>
-            
-            
-            
 
   
-            <div class="task">
+            <div className="task">
                 <input 
                   type="checkbox"
                   id="task-2"
                 />
                 <label for="task-2">
-                  <span class="custom-checkbox"></span>
+                  <span className="custom-checkbox"></span>
                   Task 2
                 </label>
-                          <div class="col-6 col-md-2">
-                  <button type="button" class="btn btn-danger">Delete</button>
+                          <div className="col-6 col-md-2">
+                  <button type="button" className="btn btn-danger">Delete</button>
               </div>
               </div>
             
             
   
-              <div class="task">
+              <div className="task">
                   <input 
                     type="checkbox"
                     id="task-3"
                   />
                   <label for="task-3">
-                    <span class="custom-checkbox"></span>
+                    <span className="custom-checkbox"></span>
                     Task 3
                   </label>
-                            <div class="col-6 col-md-2">
-                  <button type="button" class="btn btn-danger">Delete</button>
+                            <div className="col-6 col-md-2">
+                  <button type="button" className="btn btn-danger">Delete</button>
               </div>
                 </div>
             
 
-  <div class="task">
+  <div className="task">
                   <input 
                     type="checkbox"
                     id="task-4"
                   />
                   <label for="task-4">
-                    <span class="custom-checkbox"></span>
+                    <span className="custom-checkbox"></span>
                     Task 4
                   </label>
-                <div class="col-6 col-md-2">
-                  <button type="button" class="btn btn-danger">Delete</button>
+                <div className="col-6 col-md-2">
+                  <button type="button" className="btn btn-danger">Delete</button>
               </div>
                 </div>
             
             
 
-    <div class="task">
+    <div className="task">
                   <input 
                     type="checkbox"
                     id="task-5"
                   />
                   <label for="task-5">
-                    <span class="custom-checkbox"></span>
+                    <span className="custom-checkbox"></span>
                     Task 5
                   </label>
-                  <div class="col-6 col-md-2">
-                  <button type="button" class="btn btn-danger">Delete</button>
+                  <div className="col-6 col-md-2">
+                  <button type="button" className="btn btn-danger">Delete</button>
               </div>
                 </div>
            
           </div>
   
-  
+</div>  
         </div>
       </div>
       );
