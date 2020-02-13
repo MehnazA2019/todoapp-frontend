b28a05a396774907cfbbd1dccc9a07adfc40c538
Mehnaz <mehnazahmed1122@gmail.com>
2020-02-13 19:58:16 +0000

    formatted

--- src/TaskCount.js
+++ src/TaskCount.js
@@ -4,30 +4,30 @@ import React from 'react';
 import './TaskCount.css'
 
 class TaskCount extends React.Component {
-    render() {
-      return (
-          <div className="body">
- 
-        <div className="todo-body">
-        <div className="col-12 col-md-8">
-          <div className="tasks">
-            <div className="task">
-              <input 
-                type="checkbox"
-                id="task-1"
-              />
-              <label for="task-1">
-                <span className="custom-checkbox"></span>
-                Task 1
-              </label>
-                          <div className="col-6 col-md-2">
-                  <button type="button" className="btn btn-danger">Delete</button>
-              </div>
-            </div>
+  render() {
+    return (
+      <div className="body">
 
-  
-            <div className="task">
-                <input 
+        <div className="todo-body">
+          <div className="col-12 col-md-8">
+            <div className="tasks">
+              <div className="task">
+                <input
+                  type="checkbox"
+                  id="task-1"
+                />
+                <label for="task-1">
+                  <span className="custom-checkbox"></span>
+                  Task 1
+              </label>
+                <div className="col-6 col-md-2">
+                  <button type="button" className="btn btn-danger">Delete</button>
+                </div>
+              </div>
+
+
+              <div className="task">
+                <input
                   type="checkbox"
                   id="task-2"
                 />
@@ -35,65 +35,65 @@ class TaskCount extends React.Component {
                   <span className="custom-checkbox"></span>
                   Task 2
                 </label>
-                          <div className="col-6 col-md-2">
+                <div className="col-6 col-md-2">
                   <button type="button" className="btn btn-danger">Delete</button>
-              </div>
-              </div>
-            
-            
-  
-              <div className="task">
-                  <input 
-                    type="checkbox"
-                    id="task-3"
-                  />
-                  <label for="task-3">
-                    <span className="custom-checkbox"></span>
-                    Task 3
-                  </label>
-                            <div className="col-6 col-md-2">
-                  <button type="button" className="btn btn-danger">Delete</button>
-              </div>
                 </div>
-            
+              </div>
 
-  <div className="task">
-                  <input 
-                    type="checkbox"
-                    id="task-4"
-                  />
-                  <label for="task-4">
-                    <span className="custom-checkbox"></span>
-                    Task 4
+
+
+              <div className="task">
+                <input
+                  type="checkbox"
+                  id="task-3"
+                />
+                <label for="task-3">
+                  <span className="custom-checkbox"></span>
+                  Task 3
                   </label>
                 <div className="col-6 col-md-2">
                   <button type="button" className="btn btn-danger">Delete</button>
-              </div>
                 </div>
-            
-            
+              </div>
 
-    <div className="task">
-                  <input 
-                    type="checkbox"
-                    id="task-5"
-                  />
-                  <label for="task-5">
-                    <span className="custom-checkbox"></span>
-                    Task 5
+
+              <div className="task">
+                <input
+                  type="checkbox"
+                  id="task-4"
+                />
+                <label for="task-4">
+                  <span className="custom-checkbox"></span>
+                  Task 4
                   </label>
-                  <div className="col-6 col-md-2">
+                <div className="col-6 col-md-2">
                   <button type="button" className="btn btn-danger">Delete</button>
-              </div>
                 </div>
-           
+              </div>
+
+
+
+              <div className="task">
+                <input
+                  type="checkbox"
+                  id="task-5"
+                />
+                <label for="task-5">
+                  <span className="custom-checkbox"></span>
+                  Task 5
+                  </label>
+                <div className="col-6 col-md-2">
+                  <button type="button" className="btn btn-danger">Delete</button>
+                </div>
+              </div>
+
+            </div>
+
           </div>
-  
-</div>  
         </div>
       </div>
-      );
-    }
+    );
   }
-  
+}
+
 export default TaskCount;
